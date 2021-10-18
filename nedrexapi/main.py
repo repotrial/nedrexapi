e4f6d86e48081dc87e38187aed9fcba8e36a56ad
james-skelton <james.skelton@newcastle.ac.uk>
2021-10-18 19:40:40 +0100

    Added relation routes

--- nedrexapi/main.py
+++ nedrexapi/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from nedrexapi.routers import general as _general, disorder as _disorder, ppi as _ppi
+from nedrexapi.routers import general as _general, disorder as _disorder, ppi as _ppi, relations as _relations
 
 app = FastAPI(
     title="NeDRexAPI",
@@ -20,3 +20,4 @@ For a tutorial on using the API, please consult
 app.include_router(_general.router, tags=["General"])
 app.include_router(_disorder.router, prefix="/disorder", tags=["Disorder"])
 app.include_router(_ppi.router, tags=["PPI routes"])
+app.include_router(_relations.router, prefix="/relations", tags=["Relations"])
---
+++ nedrexapi/routers/relations.py
@@ -0,0 +1,89 @@
+from collections import defaultdict as _defaultdict
+from itertools import chain as _chain
+
+from fastapi import APIRouter as _APIRouter
+from pydantic import BaseModel as _BaseModel, Field as _Field
+
+from nedrexapi.db import MongoInstance
+
+router = _APIRouter()
+
+
+class DisorderSeededRequest(_BaseModel):
+    disorders: list[str] = _Field(None, title="Disorders", description="Disorders to get relationships for")
+
+
+class GeneSeededRequest(_BaseModel):
+    genes: list[str] = _Field(None, title="Genes", description="Genes to get relationships for")
+
+
+class ProteinSeededRequest(_BaseModel):
+    proteins: list[str] = _Field(None, title="Proteins", description="Proteins to get relationships for")
+
+
+@router.get("/get_encoded_proteins")
+def get_encoded_proteins(sr: GeneSeededRequest):
+    """
+    Given a set of seed genes, this route returns the proteins encoded by those genes as a hash map.
+    """
+    genes = [f"entrez.{i}" if not i.startswith("entrez") else i for i in sr.genes]
+
+    coll = MongoInstance.DB()["protein_encoded_by_gene"]
+    query = {"targetDomainId": {"$in": genes}}
+
+    results = _defaultdict(list)
+    for doc in coll.find(query):
+        gene = doc["targetDomainId"].replace("entrez.", "")
+        protein = doc["sourceDomainId"].replace("uniprot.", "")
+        results[gene].append(protein)
+
+    return results
+
+
+@router.get("/get_drugs_indicated_for_disorders")
+def get_drugs_indicated_for_disorders(dr: DisorderSeededRequest):
+    disorders = [f"mondo.{i}" if not i.startswith("mondo") else i for i in dr.disorders]
+
+    coll = MongoInstance.DB()["drug_has_indication"]
+    query = {"targetDomainId": {"$in": disorders}}
+
+    results = _defaultdict(list)
+    for doc in coll.find(query):
+        drug = doc["sourceDomainId"].replace("drugbank.", "")
+        disorder = doc["targetDomainId"].replace("mondo.", "")
+        results[disorder].append(drug)
+
+    return results
+
+
+@router.get("/get_drugs_targetting_proteins")
+def get_drugs_targetting_proteins(sr: ProteinSeededRequest):
+    proteins = [f"uniprot.{i}" if not i.startswith("uniprot.") else i for i in sr.proteins]
+
+    coll = MongoInstance.DB()["drug_has_target"]
+    query = {"targetDomainId": {"$in": proteins}}
+
+    results = _defaultdict(list)
+    for doc in coll.find(query):
+        drug = doc["sourceDomainId"].replace("drugbank.", "")
+        protein = doc["targetDomainId"].replace("uniprot.", "")
+        results[protein].append(drug)
+
+    return results
+
+
+@router.get("/get_drugs_targetting_gene_products")
+def get_drugs_targetting_gene_products(sr: GeneSeededRequest):
+    gene_products = get_encoded_proteins(sr)
+    all_proteins = list(_chain(*gene_products.values()))
+
+    sr = ProteinSeededRequest(proteins=all_proteins)
+    drugs_targetting_proteins = get_drugs_targetting_proteins(sr)
+
+    results: dict[str, list[str]] = _defaultdict(list)
+    for gene, encoded_proteins in gene_products.items():
+        for protein in encoded_proteins:
+            drugs_targetting_protein = drugs_targetting_proteins.get(protein, [])
+            results[gene] += drugs_targetting_protein
+
+    return results
