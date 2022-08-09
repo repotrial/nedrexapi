4d403b43ffc92680dc31c0e8c288b4c61896ed58
James Skelton <james.skelton@newcastle.ac.uk>
2022-08-09 16:04:50 +0000

    Added routes to static providing maps between OMIM/MONDO and ICD-10

--- nedrexapi/routers/static.py
+++ nedrexapi/routers/static.py
@@ -1,5 +1,6 @@
 import json as _json
 from enum import Enum
+from io import StringIO
 from pathlib import Path as _Path
 from urllib.request import urlopen
 
@@ -80,3 +81,35 @@ def lengths_map(x_api_key: str = _API_KEY_HEADER_ARG):
         lengths_map = f.read()
 
     return _Response(lengths_map, media_type="text/plain")
+
+
+@router.get(
+    "/icd10_omim_map",
+    summary="ICD10-OMIM map",
+)
+@check_api_key_decorator
+def icd10_omim_map(x_api_key: str = _API_KEY_HEADER_ARG):
+    with open(_STATIC_DIR / "repotrial_mappings.tsv") as f:
+        mappings = f.read()
+
+    return _Response(mappings, media_type="text/plain")
+
+
+@router.get("/icd10_mondo_map", summary="ICD10-MONDO map")
+@check_api_key_decorator
+def icd10_mondo_map(x_api_key: str = _API_KEY_HEADER_ARG):
+    # This isn't actually a static file, but putting the route here keeps it
+    # near the OMIM map
+    coll = MongoInstance.DB()["disorder"]
+    strio = StringIO()
+
+    for disorder in coll.find():
+        if not disorder["icd10"]:  # no map
+            continue
+        strio.write(f"{disorder['primaryDomainId']}\t{'|'.join(disorder['icd10'])}")
+        strio.write("\n")
+
+    strio.flush()
+    strio.seek(0)
+
+    return _Response(strio.read())
