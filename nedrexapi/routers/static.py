1ac96fe75b596e03cce1cb5c966526edcb5a51bc
James Skelton <james.skelton@newcastle.ac.uk>
2022-09-22 13:00:21 +0000

    Removed old metadata logic from static.py

--- nedrexapi/routers/static.py
+++ nedrexapi/routers/static.py
@@ -16,46 +16,6 @@ router = _APIRouter()
 _STATIC_DIR = _Path(_config["api.directories.static"])
 
 
-class VersionPart(Enum):
-    MAJOR = "major"
-    MINOR = "minor"
-    PATCH = "patch"
-
-
-class Metadata:
-    metadata = None
-
-    @classmethod
-    def parse_metadata(cls):
-        metadata_file = _STATIC_DIR / "metadata.json"
-        with metadata_file.open("r") as f:
-            cls.metadata = _json.load(f)
-
-    @classmethod
-    def write_metadata(cls):
-        metadata_file = _STATIC_DIR / "metadata.json"
-        with metadata_file.open("w") as f:
-            _json.dump(cls.metadata, f)
-
-    @classmethod
-    def increment_db_version(cls, part: VersionPart):
-        assert cls.metadata is not None
-        version = cls.metadata["version"]
-        parts = [int(i) for i in version.split(".")]
-
-        if part == VersionPart.MAJOR:
-            parts[0] += 1
-        elif part == VersionPart.MINOR:
-            parts[1] += 1
-        elif part == VersionPart.PATCH:
-            parts[2] += 1
-        else:
-            raise Exception("invalid part specified")
-
-        new_version = ".".join([str(i) for i in parts])
-        cls.metadata["version"] = new_version
-
-
 @router.get("/metadata", summary="Metadata and versions of source datasets for the NeDRex database")
 @check_api_key_decorator
 def get_metadata(x_api_key: str = _API_KEY_HEADER_ARG):
