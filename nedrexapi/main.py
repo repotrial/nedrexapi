7ee17dd6036f496892de9d64abbe072821f44b0e
AndiMajore <andi.majore@googlemail.com>
2022-11-18 19:26:45 +0100

    added api.base configuration to deploy API not only on root but also under specific path

--- nedrexapi/main.py
+++ nedrexapi/main.py
@@ -1,6 +1,6 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Request
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
@@ -49,9 +49,8 @@ For a tutorial on using the API, please consult
 """,
     version="2.0.0a",
     docs_url=None,
-    redoc_url="/",
+    redoc_url="/" if config.get("api.base") is None else config["api.base"],
 )
-
 
 if config["api.rate_limiting_enabled"]:
     from nedrexapi.common import limiter
@@ -60,22 +59,37 @@ if config["api.rate_limiting_enabled"]:
     app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
     app.add_middleware(SlowAPIMiddleware)
 
-app.include_router(_general.router, tags=["General"])
-app.include_router(_disorder.router, prefix="/disorder", tags=["Disorder"])
-app.include_router(_ppi.router, tags=["PPI routes"])
-app.include_router(_relations.router, prefix="/relations", tags=["Relations"])
-app.include_router(_graph.router, prefix="/graph", tags=["Graph"])
-app.include_router(_bicon.router, prefix="/bicon", tags=["BiCoN"])
-app.include_router(_static.router, prefix="/static", tags=["Static"])
-app.include_router(_must.router, tags=["MuST"], prefix="/must")
-app.include_router(_diamond.router, prefix="/diamond", tags=["DIAMOnD"])
-app.include_router(_domino.router, prefix="/domino", tags=["DOMINO"])
-app.include_router(_robust.router, prefix="/robust", tags=["ROBUST"])
-app.include_router(_kpm.router, prefix="/kpm", tags=["KPM"])
-app.include_router(_trustrank.router, prefix="/trustrank", tags=["TrustRank"])
-app.include_router(_closeness.router, prefix="/closeness", tags=["Closeness"])
-app.include_router(_validation.router, prefix="/validation", tags=["Validation"])
-app.include_router(_admin.router, prefix="/admin", tags=["Admin"])
-app.include_router(_variant.router, prefix="/variants", tags=["Variants"])
-app.include_router(_neo4j.router, prefix="/neo4j", tags=["Neo4j"])
-app.include_router(_comorbiditome.router, prefix="/comorbiditome", tags=["Comorbiditome & ICD10 Mapping"])
+
+def _get_prefix(base: str, prefix: str):
+    if base:
+        prefix = f"{base}{prefix}" if base != "/" else prefix
+    return prefix[:-1] if prefix.endswith("/") else prefix
+
+
+bases = ["/"]
+
+if config.get("api.base") is not None:
+    if config["api.base"] != "/":
+        bases = [config["api.base"], "/"]
+
+for app_base in bases:
+    app.include_router(_general.router, prefix=_get_prefix(app_base, "/"), tags=["General"])
+    app.include_router(_disorder.router, prefix=_get_prefix(app_base, "/disorder"), tags=["Disorder"])
+    app.include_router(_ppi.router, prefix=_get_prefix(app_base, "/"), tags=["PPI routes"])
+    app.include_router(_relations.router, prefix=_get_prefix(app_base, "/relations"), tags=["Relations"])
+    app.include_router(_graph.router, prefix=_get_prefix(app_base, "/graph"), tags=["Graph"])
+    app.include_router(_bicon.router, prefix=_get_prefix(app_base, "/bicon"), tags=["BiCoN"])
+    app.include_router(_static.router, prefix=_get_prefix(app_base, "/static"), tags=["Static"])
+    app.include_router(_must.router, prefix=_get_prefix(app_base, "/must"), tags=["MuST"])
+    app.include_router(_diamond.router, prefix=_get_prefix(app_base, "/diamond"), tags=["DIAMOnD"])
+    app.include_router(_domino.router, prefix=_get_prefix(app_base, "/domino"), tags=["DOMINO"])
+    app.include_router(_robust.router, prefix=_get_prefix(app_base, "/robust"), tags=["ROBUST"])
+    app.include_router(_kpm.router, prefix=_get_prefix(app_base, "/kpm"), tags=["KPM"])
+    app.include_router(_trustrank.router, prefix=_get_prefix(app_base, "/trustrank"), tags=["TrustRank"])
+    app.include_router(_closeness.router, prefix=_get_prefix(app_base, "/closeness"), tags=["Closeness"])
+    app.include_router(_validation.router, prefix=_get_prefix(app_base, "/validation"), tags=["Validation"])
+    app.include_router(_admin.router, prefix=_get_prefix(app_base, "/admin"), tags=["Admin"])
+    app.include_router(_variant.router, prefix=_get_prefix(app_base, "/variants"), tags=["Variants"])
+    app.include_router(_neo4j.router, prefix=_get_prefix(app_base, "/neo4j"), tags=["Neo4j"])
+    app.include_router(_comorbiditome.router, prefix=_get_prefix(app_base, "/comorbiditome"),
+                       tags=["Comorbiditome & ICD10 Mapping"])
